730552af128405d6036f6e62e614400c96426e80
elijahmm17@gmail.com <elijahmm17@gmail.com>
2018-01-11 16:15:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = "I don't do much.";
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
+<<<<<<< HEAD
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
@@ -33,4 +34,32 @@ function removeLastKitten() {
 
 function removeFirstKitten() {
   return kittens.slice(1);
+=======
+function destructivelyAppendKitten(name) {
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens = kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens = kittens.slice(1);
+  return kittens;
+}
+
+function appendKitten(name) {
+  return kittens.push(name);
+}
+
+function prependKitten(name) {
+  return kittens.unshift(name);
+>>>>>>> eb4dc7b272f6cd3388848148f7e0720d0d25a28c
 }
